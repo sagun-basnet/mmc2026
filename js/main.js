3ba2a6b99af1fb87905a3a66b436bb81c6cf1bc3
sagun basnet <basnetsagun8@gmail.com>
2026-08-21 12:27:11 +0545

    javascript

---
+++ js/main.js
@@ -0,0 +1,62 @@
+// var a = "sfdgsdfg";
+// console.log(a);
+// const b = 12;
+
+// const a = 12;
+// const b = 13;
+
+// const c = a + b;
+
+// console.log(c);
+
+// const d = 23;
+// const e = 14;
+
+// const f = d + e;
+// console.log(f);
+
+function myFun(a, b) {
+  if (a < 0 && b < 0) {
+    return 0;
+  }
+  const c = a + b;
+  return c;
+  console.log(c);
+}
+
+// const ans1 = myFun(12, 13);
+
+// console.log(myFun(12, 13));
+
+// const ans2 = myFun(23, 12);
+
+// console.log(ans1, ans2);
+
+// const arr = [
+//   1,
+//   2,
+//   3,
+//   4,
+//   5,
+//   6,
+//   7,
+//   8,
+//   9,
+//   "asdfasdf",
+//   true,
+//   1.3,
+//   undefined,
+//   null,
+//   myFun(),
+// ];
+
+// console.log(arr);
+
+// for (var i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+
+const arr1 = [1, 2, 3, 4, 5];
+const arr2 = [6, 13, 8, 17, 0];
+
+let arr3;
