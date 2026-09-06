c1ab943bce5df29dba87ac7780e9dc0ecafc6cab
sagun basnet <basnetsagun8@gmail.com>
2026-09-06 12:21:30 +0545

    hooks

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,13 +1,58 @@
+import { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
 
 const App = () => {
-  const username = "";
+  const [count, setCount] = useState(0);
+  const [show, setShow] = useState(false);
 
-  // const add = () => {};
+  const getData = () => {};
+
+  const add = () => {
+    setCount((prev) => prev + 1);
+    if (count === -1) {
+      setShow(false);
+    } else {
+      setShow(true);
+    }
+    console.log(count);
+  };
+  const sub = () => {
+    setCount((prev) => prev - 1);
+    if (count === -1) {
+      setShow(false);
+    } else {
+      setShow(true);
+    }
+    console.log(count);
+  };
+
+  useEffect(() => {
+    getData();
+    console.log("From useEffect");
+  }, []);
+
+  // const changeText = () => {
+
+  // };
 
   return (
     <>
-      <Navbar name={username} />
+      <div className="flex justify-center items-center gap-4">
+        {count !== 0 && (
+          <button onClick={sub} className="bg-red-500">
+            -
+          </button>
+        )}
+        <span>{count}</span>
+        <button onClick={add} className="bg-blue-500">
+          +
+        </button>
+      </div>
+
+      <button onClick={() => setShow(!show)} className="bg-red-500">
+        {show ? "OFF" : "ON"}
+      </button>
+      {/* <Navbar name={username} /> */}
       {/* <Navbar name="Ram" age={26} />
       <Navbar name="Ram" age={26} />
       <Navbar name="Ram" age={26} />
@@ -16,7 +61,7 @@ const App = () => {
         Hello
       </h1> */}
 
-      {username.length > 0 && <h1>Welcome {username}</h1>}
+      {/* {username.length > 0 && <h1>Welcome {username}</h1>} */}
     </>
   );
 };
