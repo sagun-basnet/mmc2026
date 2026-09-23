5b2270ff7e91a0f6e0225ae56edc816abf20e1c3
sagun basnet <basnetsagun8@gmail.com>
2026-09-23 13:14:58 +0545

    CRUS

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -4,6 +4,73 @@ export const getUser = (req, res) => {
   res.send("I am from Get Request.");
 };
 
+export const getSingleUser = (req, res) => {
+  try {
+    const { id } = req.params;
+    const q = `select * from user where id = ?`;
+
+    db.query(q, [id], (err, result) => {
+      if (err) {
+        return res.status(500).send({
+          messsage: "Error while executing query",
+          error: err,
+        });
+      }
+      return res.status(200).send({
+        data: result[0],
+      });
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const deleteUser = (req, res) => {
+  try {
+    const { id } = req.params;
+    const q = `delete from user where id = ?`;
+
+    db.query(q, [id], (err, result) => {
+      if (err) {
+        return res.send({
+          messsage: "Error while executing query",
+          error: err,
+        });
+      }
+      return res.send({
+        message: "User deleted successfully..",
+        data: result,
+      });
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const editUser = (req, res) => {
+  try {
+    const { name, phone, address, email, password } = req.body;
+    const { id } = req.params;
+
+    const q = `update user set name = ?, phone = ?, address=?, email=?, password=? where id=?`;
+
+    db.query(q, [name, phone, address, email, password, id], (err, result) => {
+      if (err) {
+        return res.send({
+          messsage: "Error while executing query",
+          error: err,
+        });
+      }
+      return res.send({
+        message: "User edited successfully",
+        data: result,
+      });
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 export const postUser = (req, res) => {
   try {
     const { name, phone, address, email, password } = req.body;
@@ -17,7 +84,6 @@ export const postUser = (req, res) => {
           error: err,
         });
       }
-
       return res.send({
         message: "User registred successfully",
         data: result,
--- backend/router/user.route.js
+++ backend/router/user.route.js
@@ -1,9 +1,18 @@
 import express from "express";
-import { getUser, postUser } from "../controllers/user.js";
+import {
+  deleteUser,
+  editUser,
+  getSingleUser,
+  getUser,
+  postUser,
+} from "../controllers/user.js";
 
 const route = express.Router();
 
 route.get("/get-user", getUser);
+route.get("/get-single-user/:id", getSingleUser);
 route.post("/post-user", postUser);
+route.delete("/delete-user/:id", deleteUser);
+route.put("/edit-user/:id", editUser);
 
 export default route;
